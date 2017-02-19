faa98dab9bea7170c002edd6882ae8d44dd0bd7e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-02-19 16:41:00 +0000

    persist state

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
-import {leanReducer} from "lean-redux";
-import {createStore} from "redux";
+import {leanReducer, composeReducers} from "lean-redux";
+import {createStore, applyMiddleware, compose} from "redux";
+import persistState from "redux-localstorage";
 
 import Main from "./Main";
 
-const store = createStore(
-    leanReducer,
-    {},
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(leanReducer, {}, composeEnhancers(persistState()));
 
 const Root = () => (
     <HashRouter>
