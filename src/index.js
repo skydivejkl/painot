aa0c73343dc116391b74e03e454f5fd00180e891
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-02-20 16:55:55 +0000

    Fix buttons on  iOS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ console.log("git date", GIT_COMMIT_DATE);
 console.log("git message", GIT_COMMIT_MESSAGE);
 console.log("git rev", GIT_COMMIT_REV);
 
+import initReactFastclick from "react-fastclick";
+initReactFastclick();
+
 import React from "react";
 import ReactDOM from "react-dom";
 import {HashRouter} from "react-router-dom";
